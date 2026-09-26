9a4cfd5b4172e10bc9c911ee5ed35cf5b3e26b2c
Thiagoo-Franca <tfranca@ufba.br>
2026-09-25 21:38:58 -0300

    fix: imports .js in index.js

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,25 +1,27 @@
-import { Hono, type MiddlewareHandler } from "hono"
-import { cors } from "hono/cors"
-import { logger } from "hono/logger"
-import { poweredBy } from "hono/powered-by"
-import { prettyJSON } from "hono/pretty-json"
-import { AppError } from "./error"
-import { appJwt } from "./modules/auth/auth.middleware"
-import { authRoutes } from "./modules/auth/auth.route"
-import { JWT_SECRET } from "./modules/auth/jwt"
-import { bancaRoutes } from "./modules/banca/banca.route"
-import { calendarRoutes } from "./modules/calendar/calendar.route"
-import { cursoRoutes } from "./modules/curso/curso.route"
-import { documentoRoutes } from "./modules/documento/documento.route"
-import { feedbackRoutes } from "./modules/feedback/feedback.route"
-import { featureRequestRoutes } from "./modules/feature-request/feature-request.route"
-import { usuarioRoutes } from "./modules/usuario/usuario.route"
-import studentInvitationRoutes from "./modules/student-invitation/student-invitation.route"
-import teacherInvitationRoutes from "./modules/teacher-invitation/teacher-invitation.route"
-import { type AppVariables } from "./types"
-import { candidatoRoutes } from "./modules/candidato/candidato.route"
+import { Hono, type MiddlewareHandler } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { poweredBy } from "hono/powered-by";
+import { prettyJSON } from "hono/pretty-json";
+import { AppError } from "./error.js";
+import { appJwt } from "./modules/auth/auth.middleware.js";
+import { authRoutes } from "./modules/auth/auth.route.js";
+import { JWT_SECRET } from "./modules/auth/jwt.js";
+import { bancaRoutes } from "./modules/banca/banca.route.js";
+import { calendarRoutes } from "./modules/calendar/calendar.route.js";
+import { cursoRoutes } from "./modules/curso/curso.route.js";
+import { documentoRoutes } from "./modules/documento/documento.route.js";
+import { feedbackRoutes } from "./modules/feedback/feedback.route.js";
+import { featureRequestRoutes } from "./modules/feature-request/feature-request.route.js";
+import { usuarioRoutes } from "./modules/usuario/usuario.route.js";
+import studentInvitationRoutes from "./modules/student-invitation/student-invitation.route.js";
+import teacherInvitationRoutes from "./modules/teacher-invitation/teacher-invitation.route.js";
+import { type AppVariables } from "./types.js";
+import { candidatoRoutes } from "./modules/candidato/candidato.route.js";
 
-export const app = (depsMiddleware: MiddlewareHandler<{ Variables: AppVariables }>) =>
+export const app = (
+  depsMiddleware: MiddlewareHandler<{ Variables: AppVariables }>,
+) =>
   new Hono<{ Variables: AppVariables }>()
     .use(depsMiddleware)
     .use("*", poweredBy())
@@ -39,14 +41,14 @@ export const app = (depsMiddleware: MiddlewareHandler<{ Variables: AppVariables 
     .route("/teacher-invitation", teacherInvitationRoutes)
     .route("/student-invitation", studentInvitationRoutes)
     .notFound((c) => {
-      return c.json({ message: "Not Found", ok: false }, 404)
+      return c.json({ message: "Not Found", ok: false }, 404);
     })
     .onError((err, c) => {
       if (err instanceof AppError) {
-        return c.json({ message: err.message }, err.status)
+        return c.json({ message: err.message }, err.status);
       }
-      console.error(`Server Error: ${err}`)
-      return c.json({ message: "Internal Server Error" }, 500)
-    })
+      console.error(`Server Error: ${err}`);
+      return c.json({ message: "Internal Server Error" }, 500);
+    });
 
-export type AppType = ReturnType<typeof app>
+export type AppType = ReturnType<typeof app>;
