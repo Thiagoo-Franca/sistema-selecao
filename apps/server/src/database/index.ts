b87f742fd45c1eb0fa76e8b5dc7739d5139802ab
Thiagoo-Franca <tfranca@ufba.br>
2026-09-25 21:17:32 -0300

    fix: imports .js

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import { env } from "../config/env";
-import runDatabaseMigrations from "./migrate";
-import * as schema from "./schema";
+import { env } from "../config/env.js";
+import runDatabaseMigrations from "./migrate.js";
+import * as schema from "./schema.js";
 
 const pool = new pg.Pool({
   connectionString: env.DATABASE_URL,
@@ -12,7 +12,7 @@ const pool = new pg.Pool({
 export const db = drizzle(pool, { schema });
 export type Database = typeof db;
 
-export * from "./schema";
-export * from "./type-utils";
+export * from "./schema.js";
+export * from "./type-utils.js";
 
 export { schema as drizzleSchema, runDatabaseMigrations };
