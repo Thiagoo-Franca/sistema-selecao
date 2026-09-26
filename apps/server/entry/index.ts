0122334b1a3acce4a8b08899bef586e3a7b7e2da
Thiagoo-Franca <tfranca@ufba.br>
2026-09-25 22:03:06 -0300

    fix: delete 'type': 'module'

--- apps/server/entry/index.ts
+++ apps/server/entry/index.ts
@@ -1,7 +1,7 @@
 import { createMiddleware } from "hono/factory";
 import { db } from "../src/database/index.js";
 import { app } from "../src/index.js";
-import type { AppVariables } from "../src/types";
+import type { AppVariables } from "../src/types.js";
 
 const TrueDeps = createMiddleware<{ Variables: AppVariables }>(
   async (c, next) => {
